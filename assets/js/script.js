ca348b8b686efa0ded5e9f025435adfec243f09f
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-09-22 17:10:26 -0400

    Add CDN for mobile responsiveness, Add drag/drop/sortable widgets Made 'trash box' droppable and functional

--- assets/js/script.js
+++ assets/js/script.js
@@ -152,6 +152,63 @@ $(".list-group").on("blur", "input[type='text']", function() {
     $(this).replaceWith(taskSpan);
 });
 
+//make list items sortable
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  // activate: function(event) {
+  //   console.log("activate", this);
+  // },
+  // deactivate: function(event) {
+  //   console.log("deactivate", this);
+  // },
+  // over: function(event) {
+  //   console.log("over", event.target);
+  // },
+  // out: function(event) {
+  //   console.log("out", event.target);
+  // },
+  update: function(event) {
+    // empty array for task data
+    var tempArr = [];
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+      var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+    // add task data to the temp array as an object
+    tempArr.push({
+      text: text,
+      date: date
+    });
+});
+      var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+    
+    //update array on task data and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  }
+});
+
 // remove all tasks
 $("#remove-tasks").on("click", function() {
     for (var key in tasks) {
@@ -163,5 +220,3 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
-
-
